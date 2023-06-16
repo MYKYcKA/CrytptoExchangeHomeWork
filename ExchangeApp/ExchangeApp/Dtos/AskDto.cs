10956b5ae562734637a941e73d0a8526e09a42ff
mykyta.dudchenko <mykyta.dudchenko@everymatrix.com>
2023-06-16 04:43:17 +0300

    DTOs

---
+++ ExchangeApp/ExchangeApp/Dtos/AskDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+[DataContract]
+public class AskDto
+{
+    [JsonPropertyName("Order")]
+    public OrderDto Order { get; set; }
+}
---
+++ ExchangeApp/ExchangeApp/Dtos/OrderDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+[DataContract]
+public class OrderDto
+{
+    [JsonPropertyName("Id")]
+    public object Id { get; set; }
+
+    [JsonPropertyName("Time")]
+    public DateTime Time { get; set; }
+
+    [JsonPropertyName("Type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("Kind")]
+    public string Kind { get; set; }
+
+    [JsonPropertyName("Amount")]
+    public double Amount { get; set; }
+
+    [JsonPropertyName("Price")]
+    public double Price { get; set; }
+}
